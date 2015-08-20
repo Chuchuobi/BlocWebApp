c070b724492bbcf325e26c1626c4bc9d13ce471a
chuchu <cobiekwe1@gmail.com>
2015-08-19 21:05:28 -0400

    initial commit

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Chuchu on 8/19/15.
+//  Copyright (c) 2015 Chuchu Obiekwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
